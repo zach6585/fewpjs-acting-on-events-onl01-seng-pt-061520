dcccb2a9ffb602e81c51c98ebfc3f91162aab9c9
zach6585 <zmines@wesleyan.edu>
2020-12-22 20:48:32 -0500

    commit tree d62695b0dca36d7cc28da80d3eacd18f207311c4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left < 180) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
